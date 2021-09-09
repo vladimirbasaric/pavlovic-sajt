e4f2fe31eebb1153b361316e854bd28ea2b5fa80
vladimirbasaric <vladimirbasaric@gmail.com>
2021-09-09 21:44:29 +0200

    zavrsio cetvorosobne stanove

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-// Template for header
+// ********* Template for my-header *************
 class MyHeader extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `
@@ -27,49 +27,8 @@ class MyHeader extends HTMLElement {
 
 customElements.define("my-header", MyHeader);
 
-// template for footer (there is footer-2 below)
+// Template for my-footer (there is my-footer2 below)
 class MyFooter extends HTMLElement {
-  connectedCallback() {
-    this.innerHTML = `
-    <footer id="footer">
-    <div class="footer-top">
-      <div class="container">
-        <div class="row">
-          <div class="col-lg-4 col-md-4 col-sm-12 text-center my-auto">
-            <div class="footer-info">
-              <h3 class="p-0 m-0">Pavlović Invest</h3>
-              <p> Vojvode Stepe 232, Beograd, Srbija </p>
-            </div>
-          </div>
-          <div class="col-lg-4 col-md-4 col-sm-12 text-center my-auto">
-            <div class="why-us mb-4">
-              <a href="contact.html" class="btn-menu animated fadeInUp scrollto">Kontaktirajte nas</a>
-            </div>
-            <div class="mx-auto text-center">
-              <a href="index.html">
-                <img src="assets/img/SHV-logo.png" alt="logo" class="img-fluid footer-logo">
-              </a>
-            </div>
-          </div>
-          <div class="col-lg-4 col-md-4 col-sm-12 text-center my-auto">
-            <div> <strong>Telefon:</strong><span> +381 69 210 3000</span><br><strong>Email:</strong><span>
-                office@pavlovicinvest.rs</span><br><strong>Web:</strong><span> www.pavlovicinvest.rs</span><br></div>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="container">
-      <div class="copyright"> &copy; Copyright 2020. Pavlović Invest. Sva prava zadržana. </div>
-    </div>
-  </footer>
-    `;
-  }
-}
-
-customElements.define("my-footer", MyFooter);
-
-// my-footer2
-class MyFooter2 extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `
     <footer id="footer">
@@ -112,9 +71,51 @@ class MyFooter2 extends HTMLElement {
   }
 }
 
+customElements.define("my-footer", MyFooter);
+
+// my-footer2
+
+class MyFooter2 extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = `
+    <footer id="footer">
+    <div class="footer-top">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-4 col-md-4 col-sm-12 text-center my-auto">
+            <div class="footer-info">
+              <h3 class="p-0 m-0">Pavlović Invest</h3>
+              <p> Vojvode Stepe 232, Beograd, Srbija </p>
+            </div>
+          </div>
+          <div class="col-lg-4 col-md-4 col-sm-12 text-center my-auto">
+            <div class="why-us mb-4">
+              <a href="contact.html" class="btn-menu animated fadeInUp scrollto">Kontaktirajte nas</a>
+            </div>
+            <div class="mx-auto text-center">
+              <a href="index.html">
+                <img src="assets/img/SHV-logo.png" alt="logo" class="img-fluid footer-logo">
+              </a>
+            </div>
+          </div>
+          <div class="col-lg-4 col-md-4 col-sm-12 text-center my-auto">
+            <div> <strong>Telefon:</strong><span> +381 69 210 3000</span><br><strong>Email:</strong><span>
+                office@pavlovicinvest.rs</span><br><strong>Web:</strong><span> www.pavlovicinvest.rs</span><br></div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="container">
+      <div class="copyright"> &copy; Copyright 2020. Pavlović Invest. Sva prava zadržana. </div>
+    </div>
+  </footer>
+    `;
+  }
+}
+
 customElements.define("my-footer2", MyFooter2);
 
-// contact
+// my-contact
 class myContact extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `
