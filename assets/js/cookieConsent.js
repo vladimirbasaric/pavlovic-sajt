73451f901c0428c710b5d9de73c0f4da089d6687
Vuk Bradic <76756b@gmail.com>
2022-11-12 12:23:47 +0100

    Cookie consent part2

--- assets/js/cookieConsent.js
+++ assets/js/cookieConsent.js
@@ -18,19 +18,38 @@ class CookieConsent extends HTMLElement {
     }
 }
 
+class NoScriptGoogleTag extends HTMLElement {
+    connectedCallback() {
+        if(localStorage.getItem("consentAccepted") == "true") {
+            //console.log('Loading <noscript> tag')
+            this.innerHTML = `
+            <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KD8D52M"
+            height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+           `
+        }
+    }
+}
+
 customElements.define("cookie-consent", CookieConsent);
+
+customElements.define("no-script-google-tag", NoScriptGoogleTag);
 
 var declineCookies = document.getElementById("declineCookies");
 var allowCookies = document.getElementById("allowCookies");
 
-declineCookies.addEventListener("click", function(){
-    console.log("cookies declined");
-    document.getElementById("cookieModalRow").remove();
-    localStorage.setItem("consentAccepted", false)
-})
+if(declineCookies != null) {
+    declineCookies.addEventListener("click", function(){
+            console.log("cookies declined");
+            document.getElementById("cookieModalRow").remove();
+            localStorage.setItem("consentAccepted", false)
+    })
+}
 
-allowCookies.addEventListener("click", function(){
-    console.log("cookies allowed");
-    document.getElementById("cookieModalRow").remove();
-    localStorage.setItem("consentAccepted", true)
-})
+if(allowCookies != null) {
+    allowCookies.addEventListener("click", function(){
+            console.log("cookies allowed");
+            document.getElementById("cookieModalRow").remove();
+            localStorage.setItem("consentAccepted", true)
+    })
+}
+
